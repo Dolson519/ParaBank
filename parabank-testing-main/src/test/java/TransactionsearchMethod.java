ee00f5bf381aff29d95cf97002e00432ca18353f
Donald Olson <89162291+Dolson519@users.noreply.github.com>
2021-12-14 16:34:46 -0500

    updates

--- parabank-testing-main/src/test/java/TransactionsearchMethod.java
+++ parabank-testing-main/src/test/java/TransactionsearchMethod.java
@@ -4,14 +4,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
+import java.util.concurrent.TimeUnit;
+
 public class TransactionsearchMethod extends Base{
 
 
         public static void TransactionSearchByAmount(){
             driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[1]/ul/li[5]/a")).click();
             driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[2]/div/div/form/div[1]/select")).click();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
             //Need to change the Xpath to correct account every time it resets
-            WebElement mainAcc = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[2]/div/div/form/div[1]/select/option[6]"));
+            WebElement mainAcc = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[2]/div/div/form/div[1]/select/option[2]"));
             mainAcc.click();
             driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[2]/div/div/form/div[8]/input")).sendKeys("20");
             driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[2]/div/div/form/div[9]/button")).click();
